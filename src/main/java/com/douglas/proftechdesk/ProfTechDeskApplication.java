5b92a244752176f86e2161685b58e8d7dbe72499
Douglas <douglas_timaofiel@hotmail.com>
2023-10-07 18:38:23 -0300

    Project started

---
+++ src/main/java/com/douglas/proftechdesk/ProfTechDeskApplication.java
@@ -0,0 +1,13 @@
+package com.douglas.proftechdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfTechDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfTechDeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/douglas/proftechdesk/ProfTechDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglas.proftechdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfTechDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
