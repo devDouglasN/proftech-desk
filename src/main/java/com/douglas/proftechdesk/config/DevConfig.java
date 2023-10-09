692d85bbf997b72d05f7930ff15202a72449de44
Douglas <douglas_timaofiel@hotmail.com>
2023-10-09 14:35:02 -0300

    Development profile created

---
+++ src/main/java/com/douglas/proftechdesk/config/DevConfig.java
@@ -0,0 +1,29 @@
+package com.douglas.proftechdesk.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.douglas.proftechdesk.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
+
+	@Bean
+	public boolean instanceDB() {
+
+		if (value.equals("create")) {
+			this.dbService.startDB();
+		}
+		return false;
+	}
+}
