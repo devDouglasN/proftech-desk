16571d8351f73bf28e65ba70de74df459637546b
Douglas <douglas_timaofiel@hotmail.com>
2023-10-09 17:47:30 -0300

    Resolved issues with serialization in Customer and Technical

--- src/main/java/com/douglas/proftechdesk/domain/Technical.java
+++ src/main/java/com/douglas/proftechdesk/domain/Technical.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.douglas.proftechdesk.domain.enums.Profile;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
@@ -15,6 +16,7 @@ public class Technical extends Person {
 	@Serial
 	private static final long serialVersionUID = 1L;
 
+	@JsonIgnore
 	@OneToMany(mappedBy = "technical")
 	private List<Ticket> tickets = new ArrayList<>();
 
