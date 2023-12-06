d520cd9849182452d48f01d8a9c48aadad909f16
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2023-12-06 00:12:26 -0300

    Updated CredentialsDTO to use a record

--- src/main/java/com/douglas/proftechdesk/domain/dtos/CredentialsDTO.java
+++ src/main/java/com/douglas/proftechdesk/domain/dtos/CredentialsDTO.java
@@ -1,23 +1,4 @@
 package com.douglas.proftechdesk.domain.dtos;
 
-public class CredentialsDTO {
-
-	private String email;
-	private String password;
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
+public record  CredentialsDTO (String email, String password) {
 }
